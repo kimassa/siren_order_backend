38e17cc4c9d0644e73234e7dc21bec0bcc85fcc7
kimassa <yeswemadeitfinally@gmail.com>
2019-08-07 18:46:40 +0900

    admin 주문취소버튼 추가

--- order/admin.py
+++ order/admin.py
@@ -18,14 +18,17 @@ class OrderAdmin(admin.ModelAdmin):
     
     list_display = ["date","status","supplier","user", "display_order_product","total_price","takeout","account_actions"]
 
-    readonly_fields = ["user","date","supplier","display_order_product","takeout","total_price"]
+    # readonly_fields = ["user","date","supplier","display_order_product","takeout","total_price"]
 
     def account_actions(self, obj):
         # import pdb; pdb.set_trace()
 
         return format_html(
-            '<a class="button" href="{}">준비 완료</a>&nbsp;',
-            reverse('admin:change-status', args=[obj.pk])
+            '<a class="button" href="{}">제품 완료</a>&nbsp;'
+            '<a class="button" href="{}">취소</a>&nbsp;'
+            ,
+            reverse('admin:change-to-ready', args=[obj.pk]),
+            reverse('admin:change-to-paid', args=[obj.pk])
         )
     account_actions.short_description = 'Account Actions'
     account_actions.allow_tags = True
@@ -34,20 +37,26 @@ class OrderAdmin(admin.ModelAdmin):
         urls = super().get_urls()
         custom_urls = [
             path(
-                '<order_id>/change_status/',
-                self.admin_site.admin_view(self.change_status),
-                name='change-status',
+                '<order_id>/change_to_ready/',
+                self.admin_site.admin_view(self.change_to_ready),
+                name='change-to-ready',
+            ),
+            path(
+                '<order_id>/change_to_paid/',
+                self.admin_site.admin_view(self.change_to_paid),
+                name='change-to-paid',
             ),
         ]
         return custom_urls + urls
 
-    def change_status(self, request, *args, **kwargs):
-        # import pdb; pdb.set_trace()
-
+    def change_to_ready(self, request, *args, **kwargs):
 
         Order.objects.filter(id=kwargs["order_id"]).update(status='PRODUCT_READY')
+        return super(OrderAdmin, self).changelist_view(request)
 
 
-        print("ffefffffffhb")
+    def change_to_paid(self, request, *args, **kwargs):
+
+        Order.objects.filter(id=kwargs["order_id"]).update(status='PAID')
         return super(OrderAdmin, self).changelist_view(request)
 
